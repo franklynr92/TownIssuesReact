5b7e353b8010c6e51c9fe07faf321fd02634364b
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-01 12:54:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Welcome to your Town's Issues
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Click here to create a category
         </a>
       </header>
     </div>
