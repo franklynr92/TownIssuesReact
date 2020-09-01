0263b66ca1efdcffb33a5ab4690c69b03e6838ce
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-01 16:23:36 -0400

    using react router inconjunction with navbar/link

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+const link = {
+  width: '100opx',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () => 
+<div> 
+<NavLink
+to ="/"
+exact
+style={link}
+activeStyle={{
+  background:'darkblue'
+}}>Home</NavLink>
+<NavLink
+to="/category"
+exact
+style={link}
+activeStyle={{
+  background:'darkblue'
+}}>Category</NavLink>
+<NavLink
+to="/issue"
+exact
+style={link}
+activeStyle={{
+  background:'darkblue'
+}}>Issue</NavLink>
+</div>
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home!</h1>
+    </div>
+  );
+};
+
+const Category = () => {
+  return (
+    <div>
+      <h1>This is my Category Component</h1>
+    </div>
+  )
+}
+
+const Issue =() => {
+  return(
+    <div>
+      <h1>This is my Issue Component</h1>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div>
+        <Navbar />
+        <Route path="/" component={Home}/>
+        <Route path="/category" component={Category}/>
+        <Route path="/issue" component={Issue}/>
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
