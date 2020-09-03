bb32a24aad8f4487ec3c9c002b5d19a5a2116c2f
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-02 21:32:49 -0400

    extracted Issue to a container

---
+++ src/Container/Issue.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Issue extends React{
+
+    render(){
+        return(
+            <div>
+              <h1>This is my Issue Component</h1>
+            </div>
+          )
+    }
+}
+
+export default Issue;
