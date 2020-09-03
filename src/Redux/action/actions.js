1b7e869f35794ed2eae6508b695d296c4252437e
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-03 14:56:30 -0400

    adding fetch to action

--- src/Redux/action/actions.js
+++ src/Redux/action/actions.js
@@ -1,7 +1,13 @@
 import C from '../constants'
 
-const fetchCategories = {
-    type: C.FETCH_CATEGORIES
+const fetchCategories = () => {
+    return (dispatch) =>{
+
+    dispatch({type: C.FETCH_CATEGORIES})
+    fetch("http://localhost:3000/categories")
+    .then(res => res.json())
+    .then(categories => console.log(categories))
+}
 
 
 }
