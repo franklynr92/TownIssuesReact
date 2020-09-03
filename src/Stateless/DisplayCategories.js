458e81db3804d09673b6123d580147e918df1239
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-03 14:08:14 -0400

    updating display on DOM

--- src/Stateless/DisplayCategories.js
+++ src/Stateless/DisplayCategories.js
@@ -5,9 +5,10 @@ const DisplayCategories = (props) => {
     return (
         
     <div>
+        <h1>Category Name</h1>
         {console.log(props.categoryData.categories.map(category => category.id))}
          {props.categoryData.categories.length !== 0
-         ? props.categoryData.categories.map(category => <h1 key={category.id}>{category.type_of_issue}</h1>)
+         ? props.categoryData.categories.map(category => <li key={category.id}>{category.type_of_issue}</li>)
         :  <button onClick={props.getCategories}>Click to get the categories</button>
         }
       
