9c9484ea59688f957cc68672f2a7d402d445a8e6
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-03 13:53:59 -0400

    removing useState, and formatting better

--- src/Stateless/DisplayCategories.js
+++ src/Stateless/DisplayCategories.js
@@ -1,30 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const DisplayCategories = (props) => {
-    const [title, SetTitle] = useState("Electrical");
-
     
-    
-    // const renderCategories = props.categoryData.map(
-    //     category => 
-    //     <h1 id={category.id}>
-    //         {category.type_of_issue}
-    //     </h1>
-    //     )
-        
-        
     return (
         
     <div>
         {console.log(props.categoryData.categories.map(category => category.id))}
          {props.categoryData.categories.length !== 0
          ? props.categoryData.categories.map(category => <h1 key={category.id}>{category.type_of_issue}</h1>)
-        : <h1>Clcik the button to see the categories</h1>
+        :  <button onClick={props.getCategories}>Click to get the categories</button>
         }
-        <button onClick={props.getCategories}>Click to get the categories</button>
+      
         <ul>
             <li>Category Name</li>
-            <li>Title: {title}</li>
         </ul>
     </div> );
 }
