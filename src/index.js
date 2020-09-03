1f3c13f86b2b7d8cfa02d57e99c63cda7e3386d6
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-03 09:07:29 -0400

    removed app from index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Category from './Container/Category'
 import Navbar from './Stateless/Navbar'
 import Home from './Home'
