05947f5c28ad592b09098d4dc69403ee00c9afa8
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-02 21:40:24 -0400

    created CategoryForm sfc

---
+++ src/Stateless/CategoryForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryForm = () => {
+    return ( 
+        <form>
+            <input type="text" name="name">Enter Category name</input>
+        </form>
+     );
+}
+ 
+export default CategoryForm;
