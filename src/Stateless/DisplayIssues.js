1af6221806eb355e8471c3179d3318db66a3572d
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-02 21:44:49 -0400

    created Display issues SFC

---
+++ src/Stateless/DisplayIssues.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const DisplayIssues = () => {
+    return ( <div>
+        <h1>Display Issues Information here</h1>
+        <table>
+            <th>Issue #here</th>
+            <tbody>
+                <td>Issue title</td>
+                <td>Issue description</td>
+                <td>Issue date</td>
+                <td>Issue cross_street_1</td>
+                <td>Issue cross_street_2</td>
+            </tbody>
+        </table>
+    </div> );
+}
+ 
+export default DisplayIssues;
