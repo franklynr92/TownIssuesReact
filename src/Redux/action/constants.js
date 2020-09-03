7b9c68ebfd50f919bcd6fc2d3f9fc429d80cdd33
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-03 14:05:55 -0400

    creating CONSTS for action and reducers

---
+++ src/Redux/action/constants.js
@@ -0,0 +1,7 @@
+const constants ={
+    ADD_CATEGORY: "ADD_CATEGORY",
+    FETCH_CATEGORIES: "FETCH_CATEGORIES",
+    ADD_ISSUE: "ADD_ISSUE",
+    FETCH_ISSUES: "FETCH_ISSUES",
+}
+export default constants;
