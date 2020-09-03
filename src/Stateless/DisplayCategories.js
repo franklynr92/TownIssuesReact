451ef9a87c18fa095de773be71c249362e854fb0
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-03 13:29:16 -0400

    displaying category forms

--- src/Stateless/DisplayCategories.js
+++ src/Stateless/DisplayCategories.js
@@ -16,11 +16,11 @@ const DisplayCategories = (props) => {
     return (
         
     <div>
-        {console.log(props)}
-         {/* {props.categoriesData.category.length !== 0
-         ? console.log(props.categoryData.map(category => category))
-        : 
-        } */}
+        {console.log(props.categoryData.categories.map(category => category.id))}
+         {props.categoryData.categories.length !== 0
+         ? props.categoryData.categories.map(category => <h1 key={category.id}>{category.type_of_issue}</h1>)
+        : <h1>Clcik the button to see the categories</h1>
+        }
         <button onClick={props.getCategories}>Click to get the categories</button>
         <ul>
             <li>Category Name</li>
