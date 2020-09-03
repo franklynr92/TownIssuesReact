efe1847bfa90bc31ea49eae283f48e08777f8451
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-02 21:37:45 -0400

    created DisplayCategories stateless

--- src/Container/Category.js
+++ src/Container/Category.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class Category extends React.Component {
 
     render(){
---
+++ src/Stateless/DisplayCategories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const DisplayCategories = () => {
+    return ( <div>
+        <ul>
+            <li>Category Name</li>
+        </ul>
+    </div> );
+}
+ 
+export default DisplayCategories;
