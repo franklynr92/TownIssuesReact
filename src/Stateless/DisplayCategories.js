27a1fb97d4d7177eadb580e1f6eb12c81249b777
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-03 15:57:03 -0400

    rendering and mapping data

--- src/Stateless/DisplayCategories.js
+++ src/Stateless/DisplayCategories.js
@@ -6,15 +6,15 @@ const DisplayCategories = (props) => {
         
     <div>
         <h1>Category Name</h1>
-        {console.log(props.categoryData.categories.map(category => category.id))}
-         {props.categoryData.categories.length !== 0
-         ? props.categoryData.categories.map(category => <li key={category.id}>{category.type_of_issue}</li>)
+        {console.log("displayCateogries props.categoryData has this:", props.categoryData.map(category => category.id))}
+         {props.categoryData.length !== 0
+         ? props.categoryData.map(category => <li key={category.id}>{category.type_of_issue}</li>)
         :  <button onClick={props.getCategories}>Click to get the categories</button>
         }
+
+
       
-        <ul>
-            <li>Category Name</li>
-        </ul>
+        
     </div> );
 }
  
