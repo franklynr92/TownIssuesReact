b86622350110aad727fdc56a51bc709ed23f5622
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-02 21:50:43 -0400

    edited display data for issues

--- src/Stateless/DisplayIssues.js
+++ src/Stateless/DisplayIssues.js
@@ -5,12 +5,20 @@ const DisplayIssues = () => {
         <h1>Display Issues Information here</h1>
         <table>
             <th>Issue #here</th>
+            <th>Issue title</th>
+            <th>Issue description</th>
+            <th>Issue date</th>
+            <th>Issue cross_street_1</th>
+            <th>Issue cross_street_2</th>
+            <th>Resolved?</th>
             <tbody>
-                <td>Issue title</td>
-                <td>Issue description</td>
-                <td>Issue date</td>
-                <td>Issue cross_street_1</td>
-                <td>Issue cross_street_2</td>
+                <td>1</td>
+                <td>Place title of issue here</td>
+                <td>Place description of issue here</td>
+                <td>Place date of issue here mm/yy</td>
+                <td>Place cross_street_1 here</td>
+                <td>Place cross_street_2 here</td>
+                <td>Place resolved true or false here</td>
             </tbody>
         </table>
     </div> );
