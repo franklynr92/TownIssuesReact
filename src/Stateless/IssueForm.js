5790db57a189aa3ccd64999e62545ab2232262aa
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-02 21:46:26 -0400

    creating IssueForm SFC

---
+++ src/Stateless/IssueForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssuesForm = () => {
+    return (
+        <form>
+            <input type="text" name="title"></input>
+        </form>
+      );
+}
+ 
+export default IssuesForm;
