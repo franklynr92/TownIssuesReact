07233be19d9e73deec58f0b3e7233c2be6e8efcd
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-03 15:57:27 -0400

    adding a debugger and loging action

--- src/Redux/reducer/reducers.js
+++ src/Redux/reducer/reducers.js
@@ -1,6 +1,9 @@
 const reducer = (state = { categories: []}, action) => {
+    
     switch(action.type){
-        case 'FETCH_CATEGORIES':
+        case 'ADD_CATEGORY':
+            // debugger;
+            console.log(action.categories)
             return{
                 ...state,
                 categories: action.categories
