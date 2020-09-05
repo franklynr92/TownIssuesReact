e678ea5cba8b5b207ad0d303d5c54bd4bd46be3b
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-05 07:52:32 -0400

    updating labels, htmlFor

--- src/Stateless/IssueForm.js
+++ src/Stateless/IssueForm.js
@@ -6,15 +6,15 @@ const IssuesForm = (props) => {
         <div>
             <br></br>
         <form>
-            <label for="title">Issue Title</label>
+            <label htmlFor="title">Issue Title</label>
             <input type="text" name="title"/><br></br>
-            <label for="description">Issue Description</label>
+            <label htmlFor="description">Issue Description</label>
             <textarea name="description" value="description" onChange={props.handleChange}></textarea><br></br>
-            <label for="date">Issue Date</label>
+            <label htmlFor="date">Issue Date</label>
             <input type="date" name="date"/><br></br>
-            <label for="issue_cross_street_1">Issue_Cross_Street_1</label>
+            <label htmlFor="issue_cross_street_1">Issue_Cross_Street_1</label>
             <input type="text" name="issue_cross_street_1"/><br></br>
-            <label for="issue_cross_street_2">Issue_Cross_Street_2</label>
+            <label htmlFor="issue_cross_street_2">Issue_Cross_Street_2</label>
             <input type="text" name="issue_cross_street_2"/><br></br>
             <button type="submit">Submit</button>
         </form>
