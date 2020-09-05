8ad79fa3e83bfa46115431aa3e5b75fd2971d2d9
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-05 07:56:24 -0400

    naming actions appropriately

--- src/Redux/action/actions.js
+++ src/Redux/action/actions.js
@@ -1,9 +1,8 @@
 import C from '../constants'
 
 export const fetchCategories = () => {
-    return (dispatch) =>{
-
-    dispatch({type: C.FETCH_CATEGORIES})
+    return (dispatch) => {
+        console.log("hitting action for fetchCategories")
     fetch("http://localhost:3000/categories")
     .then(res => res.json())
     .then(categories => dispatch({type: C.ADD_CATEGORY, categories: categories})
