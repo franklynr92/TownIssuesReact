cb9f757fc580b16ee0db6deeb4abef978d6f8695
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-05 07:54:10 -0400

    adding appropriate reducer

--- src/Redux/reducer/reducers.js
+++ src/Redux/reducer/reducers.js
@@ -1,6 +1,12 @@
 const reducer = (state = { categories: []}, action) => {
     
     switch(action.type){
+        case 'ADD_CATEGORIES':
+            console.log("hitting the reducer",action.categories)    
+        debugger;
+            
+            return{ categories: action.categories}
+
         case 'ADD_CATEGORY':
             // debugger;
             console.log(action.categories)
