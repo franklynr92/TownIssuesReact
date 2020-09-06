6c319e87a57901f50de43503f32202922998d4fe
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-06 14:22:00 -0400

    mapping out the data

--- src/Stateless/DisplayIssues.js
+++ src/Stateless/DisplayIssues.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 const DisplayIssues = (props) => {
-    console.log(props)
+  const displayTheIssues = props.issuesData.map(issue => 
+  <ul key={issue.id}>
+  <li>{issue.title}</li>
+  <li>{issue.date}</li>
+  <li>{issue.description}</li>
+  <li>{issue.cross_street_1}</li>
+  <li>{issue.cross_street_2}</li>
+  <li>{issue.resolved}</li>
+  </ul>
+  )
+
+    console.log(props.issuesData.map(issue => issue.id))
     return ( 
     <div>
-        <button onClick={props.getIssues}>Click here</button>
+       
         <h1>Display Issues Information here</h1>
+        {props.issuesData.length !== 0
+        ? displayTheIssues
+        : <button onClick={props.getIssues}>Click here</button>
+        }
         <table>
             <thead>
               <tr>
