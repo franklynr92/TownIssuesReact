9a8a4ee905e7a644827943bc1fd1584bcaf74976
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-09 12:49:35 -0400

    cleaning up code for the DOM

--- src/Stateless/DisplayCategories.js
+++ src/Stateless/DisplayCategories.js
@@ -3,22 +3,13 @@ import React from 'react';
 const DisplayCategories = (props) => {
     
     return (
-        
-    <div>
-        <h1>Category Name</h1>
-        {console.log("displayCateogries props.categoryData has the id mapped out:", props)}
-         {props.categoryData && props.categoryData.map(category => <li key={category.id}>{category.type_of_issue}</li>) }
-    <button onClick={props.getCategories}>Click to get the categories</button>
-
-      
-        
-    </div> );
+        <div>
+        <h1>Categories</h1>
+            <ul>
+            {props.categoryData && props.categoryData.map(category => <li key={category.id}>{category.type_of_issue}</li>) }
+            </ul>
+        <button onClick={props.getCategories}>Click to get the categories</button>
+        </div> 
+    );
 }
-/*
-props.issuesData.map(issue => <tr><td>{issue.id}</td>, <td>{issue.title}</td></tr>)
-
-)
-*/
-
-
 export default DisplayCategories;
