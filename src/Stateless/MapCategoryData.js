1cd59ccb17dec91339b9b13aa1acb0a0a2847051
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-14 09:27:15 -0400

    creating MapCategoryData Component

---
+++ src/Stateless/MapCategoryData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MapCategoryData = (props, {categoryData}) => {
+    console.log("mapCategoryData SFC",categoryData)
+   const categoryDatas = props.categoryData && props.categoryData.map(category => <li key={category.id}>{category.type_of_issue}</li>) 
+    return (
+        <>
+        {categoryDatas}
+        </>
+        );
+}
+ 
+export default MapCategoryData;
