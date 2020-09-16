e0bb3308b2f1d208a13fe2589eff929cec3b1f69
Franklyn Rodriguez <franklynr92@gmail.com>
2020-09-16 14:07:48 -0400

    removing unneccassry code

--- src/Stateless/MapCategoryData.js
+++ src/Stateless/MapCategoryData.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const MapCategoryData = (props, {categoryData}) => {
+const MapCategoryData = (props) => {
     console.log("mapCategoryData SFC", props, "this other thing", {props})
    const categoryDatas = props.categoryData && props.categoryData.map(category => <li key={category.id}>{category.type_of_issue}</li>) 
     return (
